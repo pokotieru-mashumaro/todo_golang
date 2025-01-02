a6f02859b9ff1bb23ef4dec2dc195f02fc741878
pokotieru-mashumaro <k827umeboshi@gmail.com>
2025-01-02 18:47:04 +0900

    init and users tabel done

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"todo_golang/app/models"
+)
+
+func main() {
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+
+	// log.Println("test")
+	fmt.Println(models.Db)
+
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "hello@gmail.com"
+	// u.PassWord = "test"
+	// fmt.Println(u)
+	// u.CreateUser()
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
+	u.Name = "test2"
+	u.Email = "test@example"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+}
